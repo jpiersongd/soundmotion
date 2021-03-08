9406d93751bc4e6b418fc76bc30604b949d5190a
jpiersongd <37314418+jpiersongd@users.noreply.github.com>
2021-03-07 21:31:16 -0800

    Create main1.py

---
+++ code/main1.py
@@ -0,0 +1,29 @@
+import time
+from random import *
+
+val1 = 0
+play = True
+
+def play_soundware():
+	print(val1)
+	
+	
+def get_agg_accel():
+	global val1
+	#decide if adding or subtracting
+	variance1 = randint(1,2)
+	#decide how much
+	variance2 = randint(0,10)
+
+	# subtract some amount
+	if variance1 ==1 and val1 - variance2 >= 0:
+		val1 = val1 - variance2
+	# add some amount
+	if variance1 > 1 and val1 + variance2 <=100:
+		val1 = val1 + variance2
+	
+	
+while play == True:
+	get_agg_accel()
+	play_soundware()
+	time.sleep(.1)
